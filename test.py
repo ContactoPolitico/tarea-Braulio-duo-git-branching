de0e046d2f1bf4a8e5b408a868d31f86f3253c3c
Saul Ruiz <srluna0205@yahoo.com>
2026-01-29 14:13:55 -0400

    termine suma y resta

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
 print("test")
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+choice = input("Type + to add or - to subtract: ")
+
+if choice == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif choice == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+else:
+    print("Invalid option")
+
