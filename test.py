6ca07346d459bbacf91d54fdbc07f82791d1b99a
Federico <alvarezacostafederico@gmail.com>
2026-01-29 13:33:14 -0500

    mult&div added

--- test.py
+++ test.py
@@ -16,3 +16,18 @@ elif choice == "-":
 else:
     print("Invalid option")
 
+num3 = int(input("Enter first number: "))
+num4 = int(input("Enter second number: "))
+
+ans = input("Type x to multiply or / to divide: ")
+
+if ans == "x":
+    finish = num3 * num4
+    print("Result:", finish)
+
+elif ans == "/":
+    finish = num3 / num4
+    print("Result:", finish)
+
+else:
+    print("Invalid option")
